06e685e8fc941fe8b96c8782e63ce0d3416fca4f
mzgubic <miha.zgubic@gmail.com>
2019-01-30 20:51:22 +0000

    fine converges, looks like there is a rogue factor of two somewhere

--- models.py
+++ models.py
@@ -1,25 +1,27 @@
 import tensorflow as tf
 from tensorflow.contrib import layers
 
-def T(x):
+def T(x, name='T'):
 
-    # need to have shared variables here!
-    # only one hidden layer
-    hidden = layers.relu(x, 10)
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+
+        # only one hidden layer
+        hidden = layers.relu(x, 10)
     
-    # output
-    output = layers.linear(hidden, 1)
+        # output
+        output = layers.linear(hidden, 1)
     
     return output
 
-def FINE(x_theta, x_theta_prime):
+def FINE(x_theta, x_theta_prime, delta_theta, name='FINE'):
     
     # compute Ts
-    T_theta = T(x_theta)
-    T_theta_prime = T(x_theta_prime)
+    T_theta = T(x_theta, name)
+    T_theta_prime = T(x_theta_prime, name)
     
     # compute the loss
     FI = tf.reduce_mean(T_theta) - tf.math.log(tf.reduce_mean(tf.math.exp(T_theta_prime)))
+    FI /= (delta_theta**2)
     loss = -FI # FI = supremum
     
     # optimiser
